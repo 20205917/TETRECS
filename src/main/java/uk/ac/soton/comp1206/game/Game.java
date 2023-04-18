feb36058a936c558f8d5977fdaaa1ca3792ae045
GaosongXu <shimaring@qq.com>
2023-04-18 10:00:56 +0800

    feat: 完成游戏块增加删除逻辑

--- src/main/java/uk/ac/soton/comp1206/game/Game.java
+++ src/main/java/uk/ac/soton/comp1206/game/Game.java
@@ -3,7 +3,9 @@ package uk.ac.soton.comp1206.game;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.ac.soton.comp1206.component.GameBlock;
+import uk.ac.soton.comp1206.component.GameBlockCoordinate;
 
+import java.util.HashSet;
 import java.util.Random;
 
 /**
@@ -28,6 +30,12 @@ public class Game {
      * The grid model linked to the game
      */
     protected final Grid grid;
+
+    /**
+     * This will keep track of the current piece
+     */
+    protected GamePiece currentPiece;
+
 
     /**
      * Create a new game with the specified rows and columns. Creates a corresponding grid model.
@@ -56,6 +64,8 @@ public class Game {
      */
     public void initialiseGame() {
         logger.info("Initialising game");
+        //When the game is initialised, spawn a new GamePiece and set it as the currentPiece
+        currentPiece = spawnPiece();
     }
 
     /**
@@ -68,15 +78,13 @@ public class Game {
         int x = gameBlock.getX();
         int y = gameBlock.getY();
 
-        //Get the new value for this block
-        int previousValue = grid.get(x, y);
-        int newValue = previousValue + 1;
-        if (newValue > GamePiece.PIECES) {
-            newValue = 0;
+        //try to place the current piece at this position
+        if (grid.canPlayPiece(currentPiece, x, y)) {
+            //If the grid can play the piece, place it
+            grid.playPiece(currentPiece, x, y);
+            nextPiece();
         }
 
-        //Update the grid with the new value
-        grid.set(x, y, newValue);
     }
 
     /**
@@ -123,7 +131,65 @@ public class Game {
         return GamePiece.createPiece(randomPiece);
     }
 
+    /**
+     * Replace the current piece with a new piece
+     */
+    public void nextPiece() {
+        currentPiece = spawnPiece();
+    }
 
+    /**
+     * afterPiece
+     * To be called after playing a piece
+     * This should clear any full vertical/horizontal lines that have been made
+     * You will need to iterate through vertical and horizontal lines, keeping
+     * track of how many lines are to be cleared and which blocks will need to be reset
+     * Any horizontal and vertical lines that have just been made should be cleared(including intersecting lines - multiple lines may be cleared at once).
+     */
+    public void afterPiece() {
+        var hashSet = new HashSet<GameBlockCoordinate>();
+        //Iterate through the grid
+        //we first iterate through the rows,find a row that is full,then we iterate through the columns to find the blocks that are full
+        //we store the coordinates of the blocks that are full in a hashset
+        //after we have iterated through the columns,we clear the row and reset the blocks that are full
+
+        for (int i = 0; i < rows; i++) {
+            var fullRow = true;
+            for (int j = 0; j < cols; j++) {
+                if (grid.get(i, j) == 0) {
+                    fullRow = false;
+                    break;
+                }
+            }
+            if (fullRow) {
+                for (int j = 0; j < cols; j++) {
+                    hashSet.add(new GameBlockCoordinate(i, j));
+                }
+            }
+        }
+
+        for (int i = 0; i < cols; i++) {
+            var fullCol = true;
+            for (int j = 0; j < rows; j++) {
+                if (grid.get(j, i) == 0) {
+                    fullCol = false;
+                    break;
+                }
+            }
+            if (fullCol) {
+                for (int j = 0; j < rows; j++) {
+                    hashSet.add(new GameBlockCoordinate(j, i));
+                }
+            }
+        }
+
+        //clear the row and reset the blocks that are full
+        for (var coordinate : hashSet) {
+            grid.set(coordinate.getX(), coordinate.getY(), 0);
+        }
+
+
+    }
 
 
 }
